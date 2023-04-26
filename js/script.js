9f3ed4293483e0474cb8c4b861899f0ad98cbfd0
Shiva <shivasgamingzone@gmail.com>
2023-04-26 18:54:56 +0530

    Updated Portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+
 // Dark Mode
 let darkmode = document.querySelector('#darkmode');
 
@@ -27,4 +32,38 @@ darkmode.onclick = () => {
         // save the user's preference in local storage
         localStorage.setItem('isDarkMode', 'false');
     }
+}
+
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
 }
